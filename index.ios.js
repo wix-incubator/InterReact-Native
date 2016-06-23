d544240ebfeafc0317bb1a3198a54b889219fc32
Aaron Greenwald <aarong@wix.com>
2016-06-23 11:06:35 +0300

    fix reference

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 import { Navigation } from 'react-native-navigation';
 
-import { registerScreens } from './screens';
+import { registerScreens } from './src/screens';
 registerScreens(); // this is where you register all of your app's screens
 
 // start the app
