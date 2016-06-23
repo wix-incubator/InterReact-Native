3f16c737fa66bc5bf427bcc46e01c9c6c3a055ed
Ethan Sharabi <ethan.shar@gmail.com>
2016-06-23 12:45:45 +0300

    add disconnect functioanlity

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -29,9 +29,7 @@ class FirebaseService {
 
   disconnect() {
     if (this.firebaseApp !== null) {
-      // this.chatNode = null;
-      // this.onChatMsgInitCallback = null;
-      // this.onChatMsgAddedCallback = null;
+      this.confNode = null;
       this.firebaseApp.off();
       this.firebaseApp = null;
     }
@@ -73,75 +71,6 @@ class FirebaseService {
       listener(snapshot.val());
     });
   }
-  // listenOnVisitorsUpdates(siteId, onVisitorsInit, onVisitorAdded, onVisitorChanged, onVisitorRemoved, onNewUnreadMessage) {
-  //   if (!this.firebaseApp) return;
-  //   const node = this.firebaseApp.child(`sites/${siteId}/visitors`);
-  //
-  //   node.on('child_added', (data) => {
-  //     onVisitorAdded(data.key(), data.val());
-  //     const chatNode = this.firebaseApp.child(`chat/${siteId}/${data.key()}`);
-  //     chatNode.off('child_added');
-  //     chatNode.orderByChild('read').equalTo(false).on('child_added', () => onNewUnreadMessage(data.key()));
-  //   });
-  //   node.on('child_changed', (data) => onVisitorChanged(data.key(), data.val()));
-  //   node.on('child_removed', (data) => onVisitorRemoved(data.key(), data.val()));
-  //   node.limitToLast(config.settings.maxVisitorsToShow).once('value', (data) => onVisitorsInit(data.val()));
-  // }
-  // listenOnSettingsUpdates(siteId, onSettingsChanged) {
-  //   if (!this.firebaseApp) return;
-  //   const node = this.firebaseApp.child(`sites/${siteId}/settings`);
-  //   node.on('value', (data) => onSettingsChanged(data.val()));
-  // }
-
-  // updateSettings(siteId, settings) {
-  //   if (!this.firebaseApp) return;
-  //   const node = this.firebaseApp.child(`sites/${siteId}/settings`);
-  //   node.update(settings);
-  // }
-
-  // verifySettingsInitialized(siteId) {
-  //   if (!this.firebaseApp) return;
-  //   const node = this.firebaseApp.child(`sites/${siteId}/settings`);
-  //   node.once('value', (data) => {
-  //     const currentSettings = data.val() || {};
-  //     const updatesToSettings = _.omitBy(defaultSettings, (value, key) => _.has(currentSettings, key));
-  //     if (!_.isEmpty(updatesToSettings)) { this.updateSettings(siteId, updatesToSettings); }
-  //   });
-  // }
-
-  // markSiteAsInstalled(siteId, installed) {
-  //   if (!this.firebaseApp) return;
-  //   const node = this.firebaseApp.child(`sites/${siteId}/installed`);
-  //   node.set(installed);
-  //   if (installed) { this.verifySettingsInitialized(siteId); }
-  // }
-
-  // listenOnNewChat(siteId, visitorId, onChatMsgInit, onChatMsgAdded) {
-  //   if (!this.firebaseApp) return;
-  //   if (this.chatNode) {
-  //     if (this.onChatMsgInitCallback) { this.chatNode.off('value', this.onChatMsgInitCallback); }
-  //     if (this.onChatMsgAddedCallback) { this.chatNode.off('child_added', this.onChatMsgAddedCallback); }
-  //   }
-  //   this.onChatMsgInitCallback = (data) => onChatMsgInit(data.val());
-  //   this.onChatMsgAddedCallback = (data) => onChatMsgAdded(data.key(), data.val());
-  //   this.chatNode = this.firebaseApp.child(`chat/${siteId}/${visitorId}`);
-  //   this.chatNode.on('child_added', this.onChatMsgAddedCallback);
-  //   this.chatNode.limitToLast(config.settings.maxOldChatsToShow).once('value', this.onChatMsgInitCallback);
-  // }
-
-  // sendChatMessage(message) {
-  //   if (!this.chatNode) return;
-  //   message.timestamp = Firebase.ServerValue.TIMESTAMP;
-  //   this.chatNode.push(message);
-  // }
-
-  // clearUnreadMessages(siteId, visitorId) {
-  //   const unreadMessagesNodes = this.firebaseApp.child(`chat/${siteId}/${visitorId}`).orderByChild('read').equalTo(false);
-  //   this.firebaseApp.child(`chat/${siteId}/${visitorId}`).transaction((currentData) => {
-  //     _.forEach(currentData, (messageNode) => messageNode.read = true);
-  //     return currentData;
-  //   });
-  // }
 }
 
 export default new FirebaseService();
